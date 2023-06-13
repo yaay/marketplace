589b46b854c118e7ef4598378726e9c1424ee936
Yehia Nabil <yehn@outlook.com>
2023-06-13 13:52:28 +0300

    Auto-generated commit [ci skip]

--- src/app/ratings.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RatingsPipe } from './ratings.pipe';
-
-describe('RatingsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RatingsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
